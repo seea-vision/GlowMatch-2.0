c4a7ed65e603fe1012e3c7ae03bb6f463275646a
seea-vision <yofame365@gmail.com>
2025-06-14 02:52:17 -0500

    Delete src/src directory

--- src/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import GlowMatchApp from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<GlowMatchApp />);
